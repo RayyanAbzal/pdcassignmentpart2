690f8338f7db53dfa032988e60b801bc2f15da2f
Rayyan Abzal <rayyanabzal@gmail.com>
2024-10-28 17:45:37 +1300

    removed unused/useless class authenticated interface implemenation

--- Service_Desk_System_GUI/src/service/desk/system/Customer.java
+++ Service_Desk_System_GUI/src/service/desk/system/Customer.java
@@ -12,7 +12,7 @@ package service.desk.system;
  * This class represents a customer in the Service Desk System.
  * It extends Person and adds a password for authentication purposes.
  */
-public class Customer extends Person implements Authenticated {
+public class Customer extends Person {
     private String password; // Holds the hashed password for the customer
 
     // Updated constructor to include first name, last name, and email
@@ -21,23 +21,19 @@ public class Customer extends Person implements Authenticated {
         this.password = password; // Set the hashed password
     }
 
-    @Override
     public String getPassword() {
         return password;
     }
 
-    @Override
     public void setPassword(String password) {
         this.password = password; // Update the hashed password
     }
 
-    @Override
     public String getUsername() {
         // Customers don't have a username, but we return the email instead for authentication purposes
         return getEmail();
     }
 
-    @Override
     public void setUsername(String username) {
         // Customers don't have a username, so this method can be left empty or log a warning
         throw new UnsupportedOperationException("Customer does not use a username. Email is used for login.");
